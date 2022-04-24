d7494c4b65f5db12dd99a098fb4a64a301e9459c
BATUHAN AYDOS <batuhanaydos12@gmail.com>
2022-04-24 20:40:55 +0300

    C# Kampı Intro

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -9,7 +9,16 @@ namespace GenericsIntro
             MyList<string> isimler = new MyList<string>();
             isimler.Add("Engin");
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
         }
     }
 }
