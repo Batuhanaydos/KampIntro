44051cb957e553b2e6f8041ad96f27f8831acd6e
BATUHAN AYDOS <batuhanaydos12@gmail.com>
2022-04-25 01:17:08 +0300

    ClassMetotIntro

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int id { get; set; }
+        public string Adi { get; set; }
+        public String Soyadi { get; set; }
+        public int TcNumarasi { get; set; }
+    }
+}
