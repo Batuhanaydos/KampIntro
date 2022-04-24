7696f4dbb328395bb4b5a115463b7dc564e1cb1c
BATUHAN AYDOS <batuhanaydos12@gmail.com>
2022-04-25 02:10:46 +0300

    MyDictonary

---
+++ Dictonary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string> dictonarys = new MyDictonary<string>();
+            dictonarys.Add("Batuhan");
+
+            Console.WriteLine(dictonarys.Length);
+
+            dictonarys.Add("Ahmet");
+
+            Console.WriteLine(dictonarys.Length);
+
+            foreach (var dictonary in dictonarys.Items)
+            {
+                Console.WriteLine(dictonary);
+            }
+        }
+    }
+}
