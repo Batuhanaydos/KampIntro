cedf5a1fb1f920a9e56b2fe58b82a303cab041b1
BATUHAN AYDOS <batuhanaydos12@gmail.com>
2023-05-31 15:49:38 +0300

    .

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.TcNumarasi);
+        }
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi : " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.TcNumarasi);
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.TcNumarasi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Batuhan";
+            musteri1.Soyadi = "Aydos";
+            musteri1.TcNumarasi = 1111111111;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ahmet";
+            musteri2.Soyadi = "Arslan";
+            musteri2.TcNumarasi = 222222222;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ayşe";
+            musteri3.Soyadi = "Alimzade";
+            musteri3.TcNumarasi = 333333333;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriListeleme(musteri2);
+            musteriManager.MusteriSilme(musteri3);
+        }
+    }
+}
